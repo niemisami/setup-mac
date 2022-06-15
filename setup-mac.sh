02bae55a622076849f5336d7b93cbe8d6f4db869
niemisami <sami@nieminen.fi>
2022-06-15 09:37:31 +0300

    Bring back accidentally deleted packages

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,16 +5,13 @@
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/niemisami/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+sudo softwareupdate --install-rosetta
+
 brew bundle --file Brewfile
 
-# Java
-sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sudo softwareupdate --install-rosetta
-
-brew bundle --file Brewfile-iterm
 echo "Install NVM" # https://github.com/nvm-sh/nvm 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
